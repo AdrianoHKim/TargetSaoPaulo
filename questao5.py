30d897c0a825a347f9b3b2a16c61585782f101a9
kimzera <adrianohkim@gmail.com>
2023-04-18 17:02:17 -0300

    questao 5 feita.

---
+++ questao5.py
@@ -0,0 +1,16 @@
+""" 5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua
+ preferência ou pode ser previamente definida no código;
+
+b) Evite usar funções prontas, como, por exemplo, reverse;
+"""
+
+string = input("Escreva uma palavra: ")
+string_reversa = ""
+
+for i in range(len(string)-1, -1, -1):
+    string_reversa += string[i]
+
+print("Palavra invertida:", string_reversa)
